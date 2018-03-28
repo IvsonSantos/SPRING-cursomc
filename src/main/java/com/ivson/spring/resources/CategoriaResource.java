9578651fcd4ebf88ac28a94800f4b6b728d8e1e2
Ivson Santos <santosivson@hotmail.com>
2018-03-27 22:44:24 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/ivson/spring/resources/CategoriaResource.java
+++ src/main/java/com/ivson/spring/resources/CategoriaResource.java
@@ -1,15 +1,29 @@
 package com.ivson.spring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ivson.spring.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")  // nome do endopoint REST
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria categoria1 = new Categoria(1, "Informatica");
+		Categoria categoria2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(categoria1);
+		lista.add(categoria2);
+		
+		return lista;
 	}
 }
