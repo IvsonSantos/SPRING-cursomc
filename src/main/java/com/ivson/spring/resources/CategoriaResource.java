e40b432172ba4206a2776f4f01515c9451fb1220
Ivson Santos <santosivson@hotmail.com>
2018-03-28 21:34:29 -0300

    Criando repository e service para Categoria

--- src/main/java/com/ivson/spring/resources/CategoriaResource.java
+++ src/main/java/com/ivson/spring/resources/CategoriaResource.java
@@ -1,29 +1,36 @@
 package com.ivson.spring.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ivson.spring.domain.Categoria;
+import com.ivson.spring.services.CategoriaServices;
 
+/**
+ * Controlador REST para servir como ENDPOINT, deve buscar do SERVICE
+ * @author Santo
+ *
+ */
 @RestController
 @RequestMapping(value = "/categorias")  // nome do endopoint REST
 public class CategoriaResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaServices service;
+	
+	/**
+	 * ENDPOINT deste método é /categorias/{id}
+	 * ResponseEntity = tipo especial para um servico REST, posui codigos HTTP de resposta
+	 * @return
+	 */
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		
-		Categoria categoria1 = new Categoria(1, "Informatica");
-		Categoria categoria2 = new Categoria(2, "Escritorio");
-		
-		List<Categoria> lista = new ArrayList<>();
-		
-		lista.add(categoria1);
-		lista.add(categoria2);
-		
-		return lista;
+		Categoria categoria = service.buscar(id);		
+		return ResponseEntity.ok().body(categoria);
 	}
 }
