519835fb4832ad4b7b3824cb678bb7c1452c3c1a
Ivson Santos <santosivson@hotmail.com>
2018-03-28 21:06:42 -0300

    Criando a primeira tabela sendo inserida via JPA no H2

--- src/main/java/com/ivson/spring/domain/Categoria.java
+++ src/main/java/com/ivson/spring/domain/Categoria.java
@@ -2,16 +2,26 @@ package com.ivson.spring.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Serializable - a classe pode ser convertida para uma sequencia de bytes ((gravar em arquivo, rede...)
  * @author Santo
  *
  */
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	
 	private String nome;
 
 	public Categoria() {
